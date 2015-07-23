3442c8e3928006190ab183ef1dd82a2422c3ee5d
Ethan Kaplan <ekaplan@ethankac5kwork2.gracenote.gracenote.com>
2015-07-23 11:15:09 -0700

    initial commit

--- doc/example-plugins/repeat.py
+++ doc/example-plugins/repeat.py
@@ -3,6 +3,8 @@ crontable = []
 outputs = []
 
 def process_message(data):
+	print data['channel']
     if data['channel'].startswith("D"):
         outputs.append([data['channel'], "from repeat1 \"{}\" in channel {}".format(data['text'], data['channel']) ])
+        print data['text']
 
